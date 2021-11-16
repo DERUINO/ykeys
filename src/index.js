890bf2918b8cc3c1574f1fc763b04acc1710ecde
Kirill Khromov <k.hromov@devspark.ru>
2021-11-16 12:55:33 +0300

    change project from webpack schematic

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as $ from 'jquery';
+import 'bootstrap';
+import 'popper.js';
+
+import '@/assets/style/style.scss';
+
+$(document).ready(() => {
+    console.log('project is ready');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,155 @@
 const path = require('path');
-const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const miniCSS = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (!IS_DEVELOPMENT) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+
+  return config;
+};
+
+const filename = ext => IS_DEVELOPMENT ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const cssLoaders = extra => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: IS_DEVELOPMENT,
+        reloadAll: true,
+      },
+    },
+    'css-loader',
+  ];
+
+  if (extra) {
+    loaders.push(extra);
+  }
+
+  return loaders;
+};
+
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env',
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+    ],
+  };
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+};
+
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions(),
+  }];
+
+  if (IS_DEVELOPMENT) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+const plugins = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: !IS_DEVELOPMENT,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+        {
+            from: './assets/images',
+            to: './assets/images',
+        },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ];
+
+  if (!IS_DEVELOPMENT) {
+    base.push(new BundleAnalyzerPlugin());
+  }
+
+  return base;
+};
 
 module.exports = {
-  entry: [
-    './src/js/index.js',
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.js'],
   },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: IS_DEVELOPMENT,
+  },
+  devtool: IS_DEVELOPMENT ? 'source-map' : '',
+  plugins: plugins(),
   module: {
     rules: [
-        {
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src/js/js'),
-            use: {
-                loader: 'babel-loader',
-            }
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                miniCSS.loader,
-                {
-                    loader: "css-loader",
-                    options: {
-                        sourceMap: true
-                    }
-                },
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        sourceMap: true,
-                    }
-                },
-                {
-                    loader: "sass-loader",
-                    options: {
-                        sourceMap: true
-                    }
-                }
-            ]
-        },
-    ]
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader'),
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-        template: 'src/index.html'
-    }),
-    new miniCSS({
-        filename: 'style.css',
-    }),
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-    }),
-    new CopyWebpackPlugin({
-        patterns: [
-            {
-                from: './src/styles/images',
-                to: './styles/images'
-            }
-        ]
-    }),
-  ],
 };
